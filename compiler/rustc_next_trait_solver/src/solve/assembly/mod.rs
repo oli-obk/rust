803e6c27a1727474e41ec00ab316dab4b3703f55
Oli Scherer <github@oli-obk.de>
2025-12-16 20:25:58 +0000

    Remove 'static requirement on try_as_dyn

--- compiler/rustc_next_trait_solver/src/solve/assembly/mod.rs
+++ compiler/rustc_next_trait_solver/src/solve/assembly/mod.rs
@@ -930,6 +930,7 @@ where
             TypingMode::Coherence => return,
             TypingMode::Analysis { .. }
             | TypingMode::Borrowck { .. }
+            | TypingMode::Reflection
             | TypingMode::PostBorrowckAnalysis { .. }
             | TypingMode::PostAnalysis => {}
         }
@@ -993,6 +994,7 @@ where
             TypingMode::Analysis { .. } => self.opaques_with_sub_unified_hidden_type(self_ty),
             TypingMode::Coherence
             | TypingMode::Borrowck { .. }
+            | TypingMode::Reflection
             | TypingMode::PostBorrowckAnalysis { .. }
             | TypingMode::PostAnalysis => vec![],
         };
